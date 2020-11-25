63a3b5226eed7a1deb197b18a1189f7b9f4e6d54
Jenseneducation-Lukas-Tucker <54941820+Jenseneducation-Lukas-Tucker@users.noreply.github.com>
2020-11-25 15:27:38 +0100

    done with webview api and fixed auto update

--- main.js
+++ main.js
@@ -1,14 +1,97 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
-
+const path = require("path");
+const fs = require("fs");
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+let initPath;
+const template = [
+  {
+    label: "Edit",
+    submenu: [
+      { role: "undo" },
+      { role: "redo" },
+      { type: "separator" },
+      { role: "cut" },
+      { role: "copy" },
+      { role: "paste" },
+      { role: "pasteandmatchstyle" },
+      { role: "delete" },
+      { role: "selectall" },
+    ],
+  },
+  {
+    label: "View",
+    submenu: [
+      { role: "reload" },
+      { role: "forcereload" },
+      { role: "toggledevtools" },
+      { type: "separator" },
+      { role: "resetzoom" },
+      { role: "zoomin" },
+      { role: "zoomout" },
+      { type: "separator" },
+      { role: "togglefullscreen" },
+    ],
+  },
+  {
+    role: "window",
+    submenu: [{ role: "minimize" }, { role: "close" }],
+  },
+];
+
+if (process.platform === "darwin") {
+  template.unshift({
+    label: app.name,
+    submenu: [
+      { role: "about" },
+      { type: "separator" },
+      { role: "services", submenu: [] },
+      { type: "separator" },
+      { role: "hide" },
+      { role: "hideothers" },
+      { role: "unhide" },
+      { type: "separator" },
+      { role: "quit" },
+    ],
+  });
+
+  // Edit menu
+  template[1].submenu.push(
+    { type: "separator" },
+    {
+      label: "Speech",
+      submenu: [{ role: "startspeaking" }, { role: "stopspeaking" }],
+    }
+  );
+
+  // Window menu
+  template[3].submenu = [
+    { role: "close" },
+    { role: "minimize" },
+    { role: "zoom" },
+    { type: "separator" },
+    { role: "front" },
+  ];
+}
+
+try {
+  data = JSON.parse(fs.readFileSync(initPath, "utf8"));
+} catch (e) {}
 
 function createWindow () {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
+    icon: path.join(__dirname, "assets/icons/png/64x64.png"),
+    backgroundColor: "#fff",
     webPreferences: {
       nodeIntegration: true,
+      // https://www.electronjs.org/docs/api/webview-tag
+      webviewTag: true, // Security warning since Electron 10
+      zoomFactor: 1.0,
+      enableRemoteModule: true,
     },
   });
   mainWindow.loadFile('index.html');
@@ -18,14 +101,26 @@ function createWindow () {
   mainWindow.once('ready-to-show', () => {
     autoUpdater.checkForUpdatesAndNotify();
   });
+
+  mainWindow.loadURL("file://" + __dirname + "/index.html");
+
+   // Display Dev Tools
+  //mainWindow.openDevTools();
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 }
 
 app.on('ready', () => {
+  initPath = path.join(app.getPath("userData"), "init.json");
   createWindow();
 });
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
+    data = {
+      bounds: mainWindow.getBounds(),
+    };
+    fs.writeFileSync(initPath, JSON.stringify(data));
     app.quit();
   }
 });
